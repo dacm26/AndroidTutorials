3d2374978207d12a60832e8e353787d11ebe2de4
Daniel Alejandro Coello Martinez <dacm26@gmail.com>
2015-05-16 13:25:05 -0600

    Finished displaying products on the menuView

--- PopeyeMesero/app/src/main/java/com/example/daniel/sunshine/app/MenuFragment.java
+++ PopeyeMesero/app/src/main/java/com/example/daniel/sunshine/app/MenuFragment.java
@@ -5,11 +5,9 @@ package com.example.daniel.sunshine.app;
  */
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -37,10 +35,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MenuFragment extends Fragment {
     private ArrayAdapter<String> mMenuadapters;
     private ArrayList<Category> categories = new ArrayList<>();
+    ArrayList<Product> products = new ArrayList<>();
     public MenuFragment() {
     }
 
@@ -60,23 +60,23 @@ public class MenuFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_refresh){
-            updateWeather();
+            updateMenu();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void updateWeather(){
-        MenuTask weatherTask = new MenuTask();
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
-        weatherTask.execute(location);
+    private void updateMenu(){
+        MenuTask menuTask = new MenuTask();
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        //String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        menuTask.execute("");
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        updateWeather();
+        updateMenu();
     }
 
     @Override
@@ -112,10 +112,11 @@ public class MenuFragment extends Fragment {
         private String[] getMenuDataFromJson(String menuJsonStr)
                 throws JSONException {
 
-            ArrayList<Product> products = new ArrayList<>();
+
             JSONArray menuJson = new JSONArray(menuJsonStr);
             JSONObject temp;
             int category=0;
+            List<String> retVal= new ArrayList<>();
             for (int i=0; i<menuJson.length(); ++i){
                 temp=menuJson.getJSONObject(i);
                 for(int j=0; j< categories.size(); ++j){
@@ -129,9 +130,10 @@ public class MenuFragment extends Fragment {
                         ,temp.getJSONObject("Image").getString("url")));
             }
             for (Product t : products){
+                retVal.add(t.toString());
                 Log.v(LOG_TAG,"Here is a product:"+t.toString());
             }
-            return null;
+            return retVal.toArray(new String[retVal.size()]);
 
         }
         @Override
